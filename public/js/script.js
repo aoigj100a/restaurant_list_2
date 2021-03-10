fc7757cc28499f0b92b35f9ab580259db0e1cfe8
aoigj100a <aoigj100@gmail.com>
2021-03-10 15:02:17 +0800

    feat:新增回到頂部按鈕

---
+++ public/js/script.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+// 回到頂端
+jsBackToTop.addEventListener("click", ()=>{
+    $('html, body').animate({ scrollTop: 0 }, 200);
+}, true);
+
+//改變收藏狀態
+// star.addEventListener("click",()=>{
+//     $('#star').toggleClass('like')
+//     $('#star').toggleClass('un-like')
+// })
+
+});
