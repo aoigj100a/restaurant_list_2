990c32372f7cb21c00eaf2e02d8be5ec10ccac82
aoigj100a <aoigj100@gmail.com>
2021-03-10 15:06:41 +0800

    feat:新增改變收藏狀態

--- public/js/script.js
+++ public/js/script.js
@@ -4,10 +4,10 @@ jsBackToTop.addEventListener("click", ()=>{
     $('html, body').animate({ scrollTop: 0 }, 200);
 }, true);
 
-//改變收藏狀態
-// star.addEventListener("click",()=>{
-//     $('#star').toggleClass('like')
-//     $('#star').toggleClass('un-like')
-// })
+// 改變收藏狀態
+star.addEventListener("click",()=>{
+    $('#star').toggleClass('like')
+    $('#star').toggleClass('un-like')
+})
 
 });
