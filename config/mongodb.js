b10b20ddc1f0e58e23479092eb3499d8ac711708
aoigj100a <aoigj100@gmail.com>
2021-03-12 12:07:45 +0800

    feat:add seeder

--- config/mongodb.js
+++ config/mongodb.js
@@ -1,5 +1,9 @@
 const mongoose = require('mongoose')
-mongoose.connect('mongodb://localhost/restaurant_list', { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect('mongodb://localhost/restaurant_list_2', { useNewUrlParser: true, useUnifiedTopology: true })
+
+// (node:2838) DeprecationWarning: collection.ensureIndex is deprecated. Use createIndexes instead.done.
+mongoose.set('useCreateIndex', true) 
+
 const db = mongoose.connection
 db.on('error', () => {
   console.log('mongoDB error!')
---
+++ models/seeds/restaurantSeeder.js
@@ -0,0 +1,17 @@
+const db = require('../../config/mongodb')
+const Restaurant = require('../restaurant')
+const restaurantList = require('../restaurant.json').results
+
+db.once('open', () => {
+    console.log('mongodb 連線了')
+    Restaurant.insertMany(restaurantList, (err, docs) => {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log('儲存成功:', docs)
+            
+        }
+        db.close() // 使用過後關閉db
+    })
+})
+
