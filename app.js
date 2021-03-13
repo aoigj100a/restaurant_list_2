09e57b44a4691de77d8bea003a338c4205d76a40
aoigj100a <aoigj100@gmail.com>
2021-03-13 12:14:22 +0800

    feat:增刪修改畫面路由

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
+const methodOverride = require('method-override') 
 
 const app = express()
 const port = 3000
@@ -9,12 +10,11 @@ app.set('view engine' , 'hbs')
 
 require('./config/mongodb')
 
+app.use(methodOverride('_method'))
 app.use(express.static('public'))
 
 const routes = require('./routes')
 app.use(routes)
-
-
 
 app.listen(port , ()=>{
     console.log(`已經連線到http://localhost:${port}`)
--- routes/modules/restaurant.js
+++ routes/modules/restaurant.js
@@ -1,12 +1,34 @@
 const express = require('express')
 const router = express.Router()
+// const Restaurant = require('../../models/restaurant')
 
 router.get('/',(req,res)=>{
     res.render('index')
 })
 
-router.get('/restaurants/1',(req,res)=>{
-    res.render('show')
+router.get('/new',(req,res)=>{
+    res.render('new')
+})
+
+//upload.single('image')
+router.post('/',(req,res)=>{
+
+})
+
+router.get('/delete/:restaurant_id',(req,res)=>{
+    res.render('delete')
+})
+router.delete('/delete/:restaurant_id',(req,res)=>{
+
+})
+
+//路線edit
+router.get('/:restaurant_id',(req, res)=>{
+    res.render('edit')
+})
+
+router.put('/:restaurant_id',(req, res)=>{
+
 })
 
 module.exports = router
