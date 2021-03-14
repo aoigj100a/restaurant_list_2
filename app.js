dcdd0ad91eb830e7873a9aa0e2285093e9344a53
aoigj100a <aoigj100@gmail.com>
2021-03-14 13:06:44 +0800

    feat:新增註冊與登入

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const express = require('express')
 const exphbs = require('express-handlebars')
 const methodOverride = require('method-override') 
 const bodyParser = require('body-parser')
+const routes = require('./routes')
+const session = require('express-session')
+const usePassport = require('./config/passport')
 
 const app = express()
 const port = 3000
@@ -15,7 +18,17 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
 app.use(express.static('public'))
 
-const routes = require('./routes')
+// 設定session
+// process.env.SESSION_SECRET,
+app.use(session({
+    secret: 'ThisIsMySecret',
+    resave: false,
+    saveUninitialized: true
+  }))
+
+
+usePassport(app)
+
 app.use(routes)
 
 app.listen(port , ()=>{
