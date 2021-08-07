f525419455b4a27a7156dd7fabce17ed7f7b0b14
aoigj100a <aoigj100@gmail.com>
2021-08-07 21:08:49 +0800

    chore: heroku init

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ if (process.env.NODE_ENV !== 'production') {
 }
 
 const app = express()
-const port = process.env.PORT
+const port = process.env.PORT || 3000
 
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
